cbf3d91d299003dbd4d41dab6df182f3e9d6a8f6
konradgnat <konradmgnat@gmail.com>
2018-11-29 22:26:59 -0600

    update

---
+++ SketchBook/RatingControl.swift
@@ -0,0 +1,17 @@
+//
+//  RatingControl.swift
+//  SketchBook
+//
+//  Created by Konrad Gnat on 11/29/18.
+//  Copyright © 2018 Konrad Gnat. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        <#code#>
+    }
+}
