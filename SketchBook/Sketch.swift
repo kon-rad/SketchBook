6159e55cbbe7769b4f6a487d18b3fe34b41a0199
konradgnat <konradmgnat@gmail.com>
2018-12-04 04:18:35 -0600

    table

--- SketchBook/Sketch.swift
+++ SketchBook/Sketch.swift
@@ -2,39 +2,23 @@
 //  Sketch.swift
 //  SketchBook
 //
-//  Created by Konrad Gnat on 12/1/18.
+//  Created by Konrad Gnat on 12/4/18.
 //  Copyright © 2018 Konrad Gnat. All rights reserved.
 //
 
 import UIKit
 
-class Sketch {
-    
-    //MARK: Properties
-    
-    var name: String
-    var photo: UIImage?
-    var rating: Int
-    
-    //MARK: Initialization
-    
-    init?(name: String, photo: UIImage?, rating: Int) {
-        // Initialization should fail if there is no name or if the rating is negative.
-        // The name must not be empty
-        guard !name.isEmpty else {
-            return nil
-        }
-        
-        // The rating must be between 0 and 5 inclusively
-        guard (rating >= 0) && (rating <= 5) else {
-            return nil
-        }
-        
-        // Initialize stored properties.
-        self.name = name
-        self.photo = photo
-        self.rating = rating
-        
+class Sketch: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
-    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
 }
