05184accc38db48dae0ffdb915e2513c439228cd
konradgnat <konradmgnat@gmail.com>
2018-12-04 01:51:18 -0600

    table

---
+++ SketchBook/Base.lproj/SketchTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SketchTableViewCell.swift
+//  SketchBook
+//
+//  Created by Konrad Gnat on 12/4/18.
+//  Copyright © 2018 Konrad Gnat. All rights reserved.
+//
+
+import UIKit
+
+class SketchTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
